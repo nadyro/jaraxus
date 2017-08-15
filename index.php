da5cf1a04ab673bf9695aa93e03c011786492785
Nadir SEHNOUN <nsehnoun@e1r3p11.42.fr>
2017-08-15 18:29:46 +0200

    progress jaraxus

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 		<link href="jaraxus.css" rel="stylesheet" />
+		<link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
@@ -13,30 +14,74 @@
 		<title>Jaraxus</title>
 </head>
 <body>
-<header>
-	<div class='col-12 div_header'>
-<div class='col-4'></div>
-<div class='col-4 container_t_l'>
-	<div class='col-5 div_title'>
+<header class='row'>
+	<div class='container div_header'>
+<div class='container container_t_l'>
+	<div class='div_title'>
 		<h2>Jaraxus</h2>
+		<h4>
+			L'apprentissage par l'amusement.
+		</h4>
 	</div>
-		<div class='col-5 div_logon'>
-		<div class='col-8 div_container_logon'>
-		<div class='div_username'>
+		<div class='container div_logon'>
+		<div class='container div_container_logon'>
+		<div class='container col-4 div_username'>
 			<input type="text" name="email_username" class="email_username" placeholder="Identifiant">	
 		</div>
-		<div class='div_pwd'>
+		<div class='container col-4 div_pwd'>
 			<input type="password" name="user_password" class="user_password" placeholder="Mot de passe">
 		</div>
+		<div class='container col-4 div_valid'>
+			<input type="button" name="valid_logon" class='valid_logon' value="Go !">
 		</div>
-		<div class='col-4 div_valid'>
-			<button name="valid_logon" class='valid_logon'> Go ! </button>
 		</div>
-	</div></div>
-<div class='col-4'></div>
+	</div>
+</div>
 	</div>
 </header>
-<div class='container div_main_index'>
+<div class='big_container'>
+<!--<div class='container div_slider'>
+	<div class='div_container_slider container'>
+
+	</div>
+</div>-->
+<div class='div_all_index'>
+<div class='div_main_tabs'>
+	<div class='div_container_tabs row'>
+		<div class='col-4 div_first_tab div_tabs'>
+		<div class='div_content_tabs'>
+			<h2 class='div_title_tabs div_title_first_tab'>
+				Les cours
+			</h2>
+			<p>
+				Une sélection de cours interactifs appropriés au niveau de chacun. Venez découvrir la richesse d'une séance d'apprentissage
+				en groupe.	
+			</p>
+			</div>
+		</div>
+		<div class='col-4 div_second_tab div_tabs'>
+			<div class='div_content_tabs'>
+			<h2 class='div_title_tabs div_title_second_tab'>
+				Les activités
+			</h2>
+			<p>
+				Une sélection d'activités aussi riches les unes que les autres. Ici, vous avez la garantie d'être rassasié au niveau de l'adrénaline. Sans oublier les bienfaits que l'apprentissage par la dépense d'énergie apporte !
+			</p>
+			</div>
+		</div>
+		<div class='col-4 div_third_tab div_tabs'>
+		<div class='div_content_tabs'>
+			<h2 class='div_title_tabs div_title_third_tab'>
+				Les programmes
+			</h2>
+			<p>
+				Nous vous proposons ici une sélection de programme diversifiés. Si l'apprentissage occupe une grande partie de notre pédagogie, nous n'oublions pas non plus les bénéfices de la diversité culturelle.
+			</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class='row div_main_index'>
 	<div class='row div_index'>
 		<div class='col-6 div_first_part'>
 
@@ -69,6 +114,8 @@
 					</p>
 		</div>
 	</div>
+</div>
+</div>
 </div>
 <footer class='div_footer'>
 
